817b973070b25d60fecfde795b227b2dff6d2f1a
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-05-01 17:02:38 +0200

    Adding subscription verification handling

--- apps/instagram_listener/src/handler.erl
+++ apps/instagram_listener/src/handler.erl
@@ -4,6 +4,9 @@
 -export([handle/2]).
 -export([terminate/3]).
 
+-define(HUB_MODE, <<"subscribe">>).
+-define(VERIFY_TOKEN, <<"token">>).
+
 init(_Transport, Req, []) ->
     {ok, Req, undefined}.
 
@@ -12,8 +15,25 @@ handle(Req, State) ->
     {ok, Req3} = reply(Method, Req2),
     {ok, Req3, State}.
 
+reply(<<"GET">>, Req) ->
+    {SubscriptionParameters, Req1} = subscription_parameters(Req),
+    case SubscriptionParameters of
+        {_, _, undefined} ->
+            cowboy_req:reply(400, Req1);
+        {?HUB_MODE, ?VERIFY_TOKEN, Challenge} ->
+            cowboy_req:reply(200, [], Challenge, Req1);
+        {_, _, _} ->
+            cowboy_req:reply(400, Req1)
+    end;
+
 reply(_, Req) ->
     cowboy_req:reply(405, Req).
 
 terminate(_Reason, _Req, _State) ->
     ok.
+
+subscription_parameters(Req) ->
+    {Mode, Req2} = cowboy_req:qs_val(<<"hub.mode">>, Req),
+    {VerifyToken, Req3} = cowboy_req:qs_val(<<"hub.verify_token">>, Req2),
+    {Challenge, Req4} = cowboy_req:qs_val(<<"hub.challenge">>, Req3),
+    {{Mode, VerifyToken, Challenge}, Req4}.
