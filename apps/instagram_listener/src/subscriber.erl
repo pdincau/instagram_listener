239e9aabe29af725687b329c408e94e0831f7778
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-05-03 20:52:31 +0200

    Refactoring to gen_server

--- apps/instagram_listener/src/subscriber.erl
+++ apps/instagram_listener/src/subscriber.erl
@@ -1,22 +1,75 @@
 -module(subscriber).
 
--export([subscribe/1, subscriptions/0, unsubscribe/0, unsubscribe/1]).
+-behaviour(gen_server).
 
+-export([start_link/0, stop/0, subscribe/1, subscriptions/0, unsubscribe/0, unsubscribe/1]).
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+             terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE).
 -define(URL, <<"https://api.instagram.com/v1/subscriptions">>).
 -define(BASE_BODY, <<"client_id={client-id}&client_secret={client-secret}&callback_url={callback_url}&verify_token={verify_token}&{params}">>).
 
+-record(state, {id, secret, callbackUrl, token}).
+
 subscribe(Subscription) ->
-    {ok, ClientId} = application:get_env(instagram_listener, client_id),
-    {ok, ClientSecret} = application:get_env(instagram_listener, client_secret),
-    {ok, CallbackUrl} = application:get_env(instagram_listener, callback_url),
-    {ok, VerifyToken} = application:get_env(instagram_listener, verify_token),
-    Body = body_for(ClientId, ClientSecret, CallbackUrl, VerifyToken, custom_body(Subscription)),
-    do_request(post, binary_to_list(?URL), Body).
+    gen_server:call(?SERVER, {subscribe, Subscription}).
 
 subscriptions() ->
-    {ok, ClientId} = application:get_env(instagram_listener, client_id),
-    {ok, ClientSecret} = application:get_env(instagram_listener, client_secret),
-    Url = url_for(ClientSecret, ClientId),
+    gen_server:call(?SERVER, subscriptions).
+
+unsubscribe() ->
+    unsubscribe(all).
+
+unsubscribe(Subscription) ->
+    gen_server:call(?SERVER, {unsubscribe, Subscription}).
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+stop() ->
+    gen_server:cast(?MODULE, stop).
+
+init([]) ->
+    {ok, Id} = application:get_env(instagram_listener, client_id),
+    {ok, Secret} = application:get_env(instagram_listener, client_secret),
+    {ok, CallbackUrl} = application:get_env(instagram_listener, callback_url),
+    {ok, Token} = application:get_env(instagram_listener, verify_token),
+    {ok, #state{id=Id, secret=Secret, callbackUrl=CallbackUrl, token=Token}}.
+
+handle_call(subscriptions, _From, #state{id=Id, secret=Secret} = State) ->
+    Reply = handle_list_subscriptions(Secret, Id),
+    {reply, Reply, State};
+
+handle_call({subscribe, Subscription}, _From, #state{id=Id, secret=Secret, callbackUrl=CallbackUrl, token=Token} = State) ->
+    Reply = handle_subscribe(Subscription, Id, Secret, CallbackUrl, Token),
+    {reply, Reply, State};
+
+handle_call({unsubscribe, Subscription}, _From, #state{id=Id, secret=Secret} = State) ->
+    Reply = handle_unsubscribe(Subscription, Secret, Id),
+    {reply, Reply, State};
+
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast(stop, State) ->
+    {stop, normal, State};
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+handle_list_subscriptions(Secret, Id) ->
+    Url = url_for(Secret, Id),
     case httpc:request(Url) of
         {ok, {{_, 200, _}, _Headers, Body}} ->
             Body;
@@ -24,26 +77,24 @@ subscriptions() ->
             {error, Error}
     end.
 
-unsubscribe() ->
-    {ok, ClientId} = application:get_env(instagram_listener, client_id),
-    {ok, ClientSecret} = application:get_env(instagram_listener, client_secret),
-    Url = url_for(ClientSecret, ClientId, <<"object=all">>),
-    do_request(delete, Url).
+handle_subscribe(Subscription, Id, Secret, CallbackUrl, Token) ->
+    Body = body_for(Id, Secret, CallbackUrl, Token, custom_body(Subscription)),
+    do_request(post, binary_to_list(?URL), Body).
 
-unsubscribe({id, Id}) when is_binary(Id) ->
-    {ok, ClientId} = application:get_env(instagram_listener, client_id),
-    {ok, ClientSecret} = application:get_env(instagram_listener, client_secret),
-    Params = <<"id={unsubscribe_id}">>,
-    BinaryParams = binary:replace(Params, <<"{unsubscribe_id}">>, Id),
-    Url = url_for(ClientSecret, ClientId, BinaryParams),
+handle_unsubscribe(all, Secret, Id) ->
+    Url = url_for(Secret, Id, <<"object=all">>),
     do_request(delete, Url);
 
-unsubscribe({object, Object}) when is_binary(Object) ->
-    {ok, ClientId} = application:get_env(instagram_listener, client_id),
-    {ok, ClientSecret} = application:get_env(instagram_listener, client_secret),
+handle_unsubscribe({object, Object}, Secret, Id) ->
     Params = <<"object={unsubscribe_object}">>,
     BinaryParams = binary:replace(Params, <<"{unsubscribe_object}">>, Object),
-    Url = url_for(ClientSecret, ClientId, BinaryParams),
+    Url = url_for(Secret, Id, BinaryParams),
+    do_request(delete, Url);
+
+handle_unsubscribe({id, Id}, Secret, Id) ->
+    Params = <<"id={unsubscribe_id}">>,
+    BinaryParams = binary:replace(Params, <<"{unsubscribe_id}">>, Id),
+    Url = url_for(Secret, Id, BinaryParams),
     do_request(delete, Url).
 
 do_request(Verb, Url) ->
