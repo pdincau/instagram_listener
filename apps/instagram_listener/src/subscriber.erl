6418caede6db7d3b20c7dba89cc1085d52184f16
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-05-03 13:29:01 +0200

    adding unsubscribe

--- apps/instagram_listener/src/subscriber.erl
+++ apps/instagram_listener/src/subscriber.erl
@@ -1,8 +1,8 @@
 -module(subscriber).
 
--export([subscriptions/0]).
+-export([subscriptions/0, unsubscribe/0, unsubscribe/1]).
 
--define(BASE_URL, <<"https://api.instagram.com/v1/subscriptions?client_secret={client-secret}&client_id={client-id}">>).
+-define(BASE_URL, <<"https://api.instagram.com/v1/subscriptions?client_secret={client-secret}&client_id={client-id}&{params}">>).
 
 subscriptions() ->
     {ok, ClientId} = application:get_env(instagram_listener, client_id),
@@ -15,7 +15,33 @@ subscriptions() ->
             {error, Error}
     end.
 
+unsubscribe() ->
+    do_request(delete, <<"&object=all">>).
+
+unsubscribe({id, Id}) when is_binary(Id) ->
+    Params = <<"id={unsubscribe_id}">>,
+    do_request(delete, binary:replace(Params, <<"{unsubscribe_id}">>, Id));
+
+unsubscribe({object, Tag}) when is_binary(Tag) ->
+    Params = <<"tag={unsubscribe_tag}">>,
+    do_request(delete, binary:replace(Params, <<"{unsubscribe_tag}">>, Tag)).
+
+do_request(Verb, Params) ->
+    {ok, ClientId} = application:get_env(instagram_listener, client_id),
+    {ok, ClientSecret} = application:get_env(instagram_listener, client_secret),
+    Url = url_for(ClientSecret, ClientId, Params),
+    case httpc:request(Verb, {Url, [], "", <<"">>}, [], []) of
+        {ok, {{_, 200, _}, _Headers, Body}} ->
+            Body;
+        Error ->
+            {error, Error}
+    end.
+
 url_for(ClientSecret, ClientId) ->
+    url_for(ClientSecret, ClientId, <<"">>).
+
+url_for(ClientSecret, ClientId, Params) ->
     Url = binary:replace(?BASE_URL, <<"{client-secret}">>, ClientSecret),
     Url1 = binary:replace(Url, <<"{client-id}">>, ClientId),
-    binary_to_list(Url1).
+    Url2 = binary:replace(Url1, <<"{params}">>, Params),
+    binary_to_list(Url2).
