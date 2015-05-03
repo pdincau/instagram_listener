41797fb59454f1eea214b50083e76321fe5a80dc
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-05-03 21:07:05 +0200

    Improving file

--- apps/instagram_listener/src/client.erl
+++ apps/instagram_listener/src/client.erl
@@ -1,11 +1,9 @@
 -module(client).
-
 -behaviour(gen_server).
 
 -export([start_link/0, stop/0, subscribe/1, subscriptions/0, unsubscribe/0, unsubscribe/1]).
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-             terminate/2, code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -define(SERVER, ?MODULE).
 -define(URL, <<"https://api.instagram.com/v1/subscriptions">>).
